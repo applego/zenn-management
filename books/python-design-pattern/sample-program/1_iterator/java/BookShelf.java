b7b844bafe3a34b8c91a3497e04559344f24978f
applego <applegorillappa@gmail.com>
2023-04-12 18:12:50 +0900

    問題1の解答(java)

--- books/python-design-pattern/sample-program/1_iterator/java/BookShelf.java
+++ books/python-design-pattern/sample-program/1_iterator/java/BookShelf.java
@@ -1,18 +1,18 @@
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
-    private int last = 0;
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    private ArrayList books;
+    public BookShelf(int initialsize) {
+        this.books = new ArrayList(initialsize);
     }
     public Book getBookAt(int index) {
-        return books[index];
+        return (Book)books.get(index);
     }
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        books.add(book)
     }
     public int getLength() {
-        return last;
+        return books.size();
     }
     public Iterator iterator() {
         return new BookShelfIterator(this);
